721a8c1e9e424df24b7f0939e75646ed8d51ab6c
lethekhoi <lethekhoi@gmail.com>
2020-04-01 01:42:52 +0700

    custom giao diện Trang Chủ (toolbar+thanh tìm kiếm+tablayout+viewpager)

--- app/src/main/java/com/example/applazada/View/TrangChu/TrangChuActivity.java
+++ app/src/main/java/com/example/applazada/View/TrangChu/TrangChuActivity.java
@@ -1,9 +1,13 @@
 package com.example.applazada.View.TrangChu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.applazada.Adapter.ViewPagerAdapter;
 import com.example.applazada.R;
@@ -12,15 +16,33 @@ import com.google.android.material.tabs.TabLayout;
 public class TrangChuActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tabLayout;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
         viewPager = findViewById(R.id.viewpager);
+        toolbar = findViewById(R.id.toolbarTrangChu);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
         tabLayout = findViewById(R.id.tabs);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutrangchu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
