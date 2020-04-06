dc89ee57d6ba8d2ec706c64a98a9e6480913633c
lethekhoi <lethekhoi@gmail.com>
2020-04-07 00:03:02 +0700

    xử lý đăng nhập bằng Facebook

--- app/src/main/java/com/example/applazada/View/DangNhap/Fragment/FragmentDangNhap.java
+++ app/src/main/java/com/example/applazada/View/DangNhap/Fragment/FragmentDangNhap.java
@@ -1,21 +1,76 @@
 package com.example.applazada.View.DangNhap.Fragment;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.applazada.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class FragmentDangNhap extends Fragment {
+    Button btnDangNhapFacebook;
+    CallbackManager callbackManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.layout_fragment_dang_nhap, container, false);
+        FacebookSdk.sdkInitialize(getContext().getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Kiemtra", "Thành công");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("Kiemtra", "Thoát");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("Kiemtra", "lỗi");
+
+            }
+        });
+        btnDangNhapFacebook = view.findViewById(R.id.btnDangNhapFacebook);
+        btnDangNhapFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(FragmentDangNhap.this,Arrays.asList("public_profile"));
+            }
+        });
         return view;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
