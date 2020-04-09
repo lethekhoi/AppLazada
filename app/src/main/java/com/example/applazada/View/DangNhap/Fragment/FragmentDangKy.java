e4d83225e48126f175570b5d4655369361c22a99
lethekhoi <lethekhoi@gmail.com>
2020-04-09 23:51:26 +0700

    custom fragment đăng ký

--- app/src/main/java/com/example/applazada/View/DangNhap/Fragment/FragmentDangKy.java
+++ app/src/main/java/com/example/applazada/View/DangNhap/Fragment/FragmentDangKy.java
@@ -9,10 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.applazada.R;
+
 public class FragmentDangKy extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.layout_fragment_dang_ky, container, false);
+        return view;
     }
 }
